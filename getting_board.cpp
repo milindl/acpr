09761090f04a596399385c5f59752f20714f7d5c
Milind L <milindl@iitk.ac.in>
2015-12-01 23:55:20 +0530

    This gets the chessboard into a friendly array that we can use

--- getting_board.cpp
+++ getting_board.cpp
@@ -7,14 +7,86 @@
 using namespace std;
 using namespace cv;
 
-Mat src,hsv, holded, holded1; 
-Mat final1, final2; 
-int flag=0, flag2=0; 
 
-int main() {
-	VideoCapture cam(1); 
-	char key; 
-	int maxArea=0, minArea=0; 
+/*global variables to be used thru the function */ 
+
+int min_area=0, max_area=0; 
+int min_angle = 15, max_angle=75; 
+float min_ratio = 0.8, max_ratio=1.2;
+//To be used for proc
+Mat src,hsv, holded, holded1; 
+
+//To be used for printing
+Mat final1, final2; 
+int flag=0, flag2=0;
+ 
+RotatedRect *label(VideoCapture); 
+
+int main() { 
+	VideoCapture cam(1);
+	Mat src1; 
+	cam.read(src1); 
+	Mat draw1 = Mat::zeros(src1.size(), CV_8UC3); 
+	RotatedRect *r = label(cam); 
+	for(int i= 0; i!=64; i++) {
+		string s;
+		stringstream out;
+		out << i;
+		s = out.str();
+		putText(draw1, s, r[i].center, 1,1, Scalar(0,0,255)); 
+			
+	}
+	imshow("..", src1+draw1); 
+	waitKey(0);
+} 
+
+/* This function checks if a contour is a valid chess block */
+bool checkContour(vector<Point> cont) {
+	Moments m = moments(cont, false); 
+	//Area: 
+	if(m.m00>max_area || m.m00<min_area) return false; 
+	//Angle
+	RotatedRect r = minAreaRect( Mat(cont));
+	float modAngle = r.angle>0?r.angle:-r.angle; 	
+	if(modAngle>min_angle && modAngle<max_angle) return false; 
+	//Sides Ratio
+	float ht = r.size.height; 
+	float wt = r.size.width; 
+	float ratio = ht/wt;
+	if(ratio<min_ratio || ratio>max_ratio) return false; 
+	//It is alright
+	return true;
+}
+/* This sorts the array of rotated rects */ 
+RotatedRect *sorting(RotatedRect *original, int N) {
+	int k = N/2; 
+	int flag = 0; 
+	int k2=0; 
+	RotatedRect *sorted = (RotatedRect *) malloc(sizeof(RotatedRect) * N); 
+	for(int i=0; i!=k; i++) {
+		if(flag==0) {
+		sorted[2*i] = original[32+i]; 
+		sorted[2*i+1] = original[i]; 
+		k2+=2;  
+		} else{
+		sorted[2*i] = original[i]; 
+		sorted[2*i+1] = original[32+i]; 
+		k2+=2;  
+		}
+		if(k2==8) {
+			flag=!flag; 
+			k2=0;
+		}	
+	}
+	return sorted; 
+}
+
+/* This function returns an array of RotatedRects containing the chess blocks */ 
+RotatedRect *label(VideoCapture cam) {
+	char key; //to quit
+	RotatedRect *rects = (RotatedRect *)malloc(64 * sizeof(RotatedRect)); 
+
+	/* These are configuration trackbars. Will be removed once the chess board is final. */ 
 	namedWindow("Value and Saturation"); 
 	namedWindow("Value and Saturation1"); 
 	int vL=0, vH=0, sL=0, sH=0; 
@@ -27,16 +99,20 @@ int main() {
 	createTrackbar("sL1", "Value and Saturation1", &sL1, 255); 
 	createTrackbar("vH1", "Value and Saturation1", &vH1, 255); 
 	createTrackbar("vL1", "Value and Saturation1", &vL1, 255); 
-	createTrackbar("minArea", "Value and Saturation", &minArea, 10000); 
-	createTrackbar("maxArea", "Value and Saturation", &maxArea, 10000); 
+	createTrackbar("minArea", "Value and Saturation", &min_area, 10000); 
+	createTrackbar("maxArea", "Value and Saturation", &max_area, 10000); 
+	/* end config */
+	//Start main loop: 
 	while(key!=27) {
+		//Read and threshold image. Clean up. 
 		cam.read(src);
 		cvtColor(src,hsv, CV_BGR2HSV); 
 		inRange(hsv, Scalar(0, sL, vL), Scalar(179, sH,vH),holded); 
 		inRange(hsv, Scalar(0, sL1, vL1), Scalar(179, sH1,vH1),holded1); 
-		//Canny(		
 		medianBlur(holded, holded, 15); 
-		medianBlur(holded1, holded1, 15); 		
+		medianBlur(holded1, holded1, 15);
+
+		//Fiond contours for black and white , find moments		
 		vector<vector<Point> > contours; 		
 		vector<Vec4i> hierarchy;	
 		vector<vector<Point> > contours1; 		
@@ -54,28 +130,28 @@ int main() {
 				
 		vector<Point2f> mc( contours.size() );
 		vector<Point2f> mc1( contours1.size() );
+
+
 		for( int i = 0; i < contours.size(); i++ ) {
 			mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
 		}
 		for( int i = 0; i < contours1.size(); i++ ) {
 			mc1[i] = Point2f( mu1[i].m10/mu1[i].m00 , mu1[i].m01/mu1[i].m00 ); 
 		}
-		Mat drawing = Mat::zeros( holded.size(), CV_8UC3 );
-		vector<Rect> boundRect( contours.size() );
-		int k=0; 
+
+		
+		Mat drawing = Mat::zeros( holded.size(), CV_8UC3 );//for drawing
+
+		
+		int k=0; //k will count number of white/black boxes which are valid. 
 		for( int i = 0; i< contours.size(); i++ )
      		{
 
-			if(mu[i].m00<minArea || mu[i].m00>maxArea) continue; 
+			
 			if(flag==1) continue; 
-			//Rect r = boundingRect(contours[i]); 
+			if(checkContour(contours[i])==false) continue; 
 			RotatedRect r1 = minAreaRect( Mat(contours[i])); 	
-			float modAngle = r1.angle>0?r1.angle:-r1.angle; 
-			float ht = r1.size.height; 
-			float wt = r1.size.width; 
-			float ratio = ht/wt;
-			if(modAngle<75 && modAngle>15) continue; 	
-			if(ratio<0.8 || ratio >1.2) continue;
+			/* start drawing functions */
 			Point2f r[4]; r1.points(r);
 			for( int j = 0; j < 4; j++ ){ 
           			line( drawing, r[j], r[(j+1)%4], Scalar(255,255,0), 2, 8 ); 
@@ -86,29 +162,25 @@ int main() {
 			out << k;
 			s = out.str();
 			putText(drawing, s, mc[i], 1,1, Scalar(255,255,255)); 
+			rects[k] = r1; 
+			/* end drawing functions */ 
 			k++;  			
-			if(k==32) {
+			if(k==32) { //k=32 implies we are finished with the chess blocks
 				flag=1; 
 				drawing.copyTo(final1); 
-				}
-			//rectangle(drawing, r, Scalar(0,255,255)); 
-	//		drawContours( drawing, contours, i, Scalar(0,0,255), 2, 8, hierarchy, 0, Point());       			
-			//circle( drawing, mc[i], 4, Scalar(0,255,255), -1, 8, 0 );
+			}
+	
      		}	
-		k=0;	
+
+		k=0;	//reset white/black counter for black 
 		for( int i = 0; i< contours1.size(); i++ )
      		{
 
-			if(mu1[i].m00<minArea || mu1[i].m00>maxArea) continue; 
-			if(flag2==1) continue; 
-			//Rect r = boundingRect(contours[i]); 
+			if(flag2==1) continue;			
+			if(checkContour(contours1[i])==false) continue; 
 			RotatedRect r1 = minAreaRect( Mat(contours1[i])); 
-			float modAngle = r1.angle>0?r1.angle:-r1.angle; 
-			float ht = r1.size.height; 
-			float wt = r1.size.width; 
-			float ratio = ht/wt;
-			if(modAngle<75 && modAngle>15) continue; 
-			if(ratio<0.8 || ratio>1.2) continue;	
+
+			/* start drawing functions */	
 			Point2f r[4]; r1.points(r);
 			for( int j = 0; j < 4; j++ ){ 
           			line( drawing, r[j], r[(j+1)%4], Scalar(0,0,255), 2, 8 );
@@ -118,20 +190,21 @@ int main() {
 			out << k;
 			s = out.str();
 			putText(drawing, s, mc1[i], 1,1, Scalar(255,0,255)); 
+			rects[k+32] = r1; 
+			/* end drawing function */ 
+
 			k++;  		
 			if(k==32) {
-			flag2=1;
-			drawing.copyTo(final2);
+				flag2=1;
+				drawing.copyTo(final2);
 			}	
-			//rectangle(drawing, r, Scalar(0,255,255)); 
-	//		drawContours( drawing, contours, i, Scalar(0,0,255), 2, 8, hierarchy, 0, Point());       			
-			//circle( drawing, mc[i], 4, Scalar(0,255,255), -1, 8, 0 );
      		}
 		
-		imshow("COM", src+drawing); 
-		//imshow("Thresh", holded); 
+		imshow("COM", src+drawing); //Display current drawn imgs
 		key=waitKey(30); 
 	}
-	imshow("final", src+final2 + final1); 
+	imshow("final", src+final2 + final1); //display the final contours 
+	
 	waitKey(0); 
+	return sorting(rects, 64);
 }
